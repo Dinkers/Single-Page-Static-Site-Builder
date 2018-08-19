13aae71d24b80afdf75d11762d746d370429af1b
Kayra <KayraAlat@gmail.com>
2018-08-19 16:37:20 +0300

    Added set up and structure to bash script

--- build.sh
+++ build.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
+
+#### Set up ####
+
 read -p "Project name: " project_name
 read -p "Author: " author
+read -p "Directory to build in: " build_directory
 read -p "Github repository url: " github_url
 
-echo "Building $project_name for $author."
+project_name_directory=$(echo $project_name | tr '[:upper:]' '[:lower:]' | tr -s ' ' | tr ' ' '_')
+
+if [ ${build_directory:0:1} == "." ]
+then
+    full_build_directory=$build_directory$project_name_directory
+else
+    full_build_directory=$build_directory/$project_name_directory
+fi
+
+echo "Building $project_name for $author in $full_build_directory"
+
+
+#### Build ####
+
+mkdir $full_build_directory
+
+
+
+####  npm  #####
+
+
+
+#### Github ####
+
+
+#### Grunt #####
