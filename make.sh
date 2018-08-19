ba3d4d22ff844a3e8f36417c7425a0e00220e659
Kayra <KayraAlat@gmail.com>
2018-08-19 18:38:58 +0300

    Adding building

--- make.sh
+++ make.sh
@@ -4,4 +4,14 @@ read project_name author build_directory github_url project_name_directory full_
 
 
 echo "Building $project_name for $author in $full_build_directory"
-bash scripts/build.sh $full_build_directory $project_name
+bash scripts/build.sh $project_name $author $build_directory $github_url $full_build_directory
+
+if [ $? -eq 0 ]
+then
+    echo "Successfully built files for $project_name"
+else
+    echo "Could not build files for $project_name" >&2
+    echo "Cleaning up"
+    rm -rf "$full_build_directory"
+    exit 1
+fi
--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,15 @@
 
 mkdir $full_build_directory
 
-cp ./build_files/* $full_build_directory
+cp ./build_files/* "$full_build_directory"
 
+# build package.json
 sed -i.bak "s/PROJECT_NAME/$project_name/g" "$full_build_directory/package.json" && rm "$full_build_directory/package.json.bak"
+sed -i.bak "s/AUTHOR/$author/g" "$full_build_directory/package.json" && rm "$full_build_directory/package.json.bak"
+sed -i.bak "s/GITHUB_URL/$github_url/g" "$full_build_directory/package.json" && rm "$full_build_directory/package.json.bak"
+
+# build index.html
+sed -i.bak "s/PROJECT_NAME/$project_name/g" "$full_build_directory/index.html" && rm "$full_build_directory/index.html.bak"
+
+# build README.md
+sed -i.bak "s/PROJECT_NAME/$project_name/g" "$full_build_directory/README.md" && rm "$full_build_directory/README.md.bak"
