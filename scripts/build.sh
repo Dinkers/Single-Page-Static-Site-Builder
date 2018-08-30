6f32f76c0f832567b0379804880a507d5f96248d
Kayra <KayraAlat@gmail.com>
2018-08-30 20:00:57 +0100

    Variable passing bug fixes

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-project_name=$1
+project_name_directory=$1
 author=$2
 full_build_directory=$3
 github_url=$4
@@ -14,12 +14,12 @@ mkdir -p "$full_build_directory"
 cp -R ./build_files/* "$full_build_directory"
 
 # build package.json
-sed -i.bak "s/PROJECT_NAME/$project_name/g" "$full_build_directory/package.json" && rm "$full_build_directory/package.json.bak"
+sed -i.bak "s/PROJECT_NAME/$project_name_directory/g" "$full_build_directory/package.json" && rm "$full_build_directory/package.json.bak"
 sed -i.bak "s/AUTHOR/$author/g" "$full_build_directory/package.json" && rm "$full_build_directory/package.json.bak"
 sed -i.bak "s/GITHUB_URL/$github_url/g" "$full_build_directory/package.json" && rm "$full_build_directory/package.json.bak"
 
 # build index.html
-sed -i.bak "s/PROJECT_NAME/$project_name/g" "$full_build_directory/index.html" && rm "$full_build_directory/index.html.bak"
+sed -i.bak "s/PROJECT_NAME/$project_name_directory/g" "$full_build_directory/index.html" && rm "$full_build_directory/index.html.bak"
 
 # build README.md
-sed -i.bak "s/PROJECT_NAME/$project_name/g" "$full_build_directory/README.md" && rm "$full_build_directory/README.md.bak"
+sed -i.bak "s/PROJECT_NAME/$project_name_directory/g" "$full_build_directory/README.md" && rm "$full_build_directory/README.md.bak"
